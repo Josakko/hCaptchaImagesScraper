67578ac2e2b0820c51cb81a2c87b5ef2885e333c
Josakko <112590914+Josakko@users.noreply.github.com>
2023-10-21 12:11:36 +0200

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+cd src/ || exit
+
+mkdir build/
+cd build/ || exit
+mkdir assets/
+cp ../assets/icon.png assets/icon.png
+cd ..
+
+rm -rf venv
+python3 -m venv venv
+source venv/bin/activate
+
+curl -o req.txt https://raw.githubusercontent.com/Josakko/hCaptchaImagesScraper/main/requirements.txt
+pip3 install -r req.txt
+
+python3 -m nuitka main.py --clang --disable-console --clean-cache=all --remove-output --output-dir=build --onefile --standalone --enable-plugins=tk-inter # --follow-imports
+
+deactivate
+rm -rf venv
+rm -rf req.txt
+
+mv build/main.bin build/hChaptchaImageScraper
+
+echo "Compiling finished!"
